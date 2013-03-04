0305516b90151eb844bf7bcf822b0cba42ae79d1
parryc <parrycadwallader@gmail.com>
2013-03-04 17:52:43 -0500

    Switch text processing to avoid internal divs

--- verticaltext.js
+++ verticaltext.js
@@ -4,7 +4,10 @@
 				lineLength: 20,
 				noOutsideWrap: true
 			},
-		rawHtml = document.querySelectorAll(options.selector)[0].innerHTML.trim();
+		selected = document.querySelectorAll(options.selector)[0],
+		rawHtml = selected.innerHTML.trim(),
+		children = selected.childNodes;
+
 
 	String.prototype.reverse=function(){return this.split("").reverse().join("");};
 
@@ -12,6 +15,7 @@
 		return {
 			options: options,
 			rawHtml: rawHtml,
+			children: children,
 			configure: function(opts){
 				for(var prop in opts){
 					if(opts.hasOwnProperty(prop))
@@ -27,24 +31,31 @@
 			},
 			setVertical: function(){
 				var split = this.rawHtml.split(/<br\/?>/),
+					children = this.children,
 					build = "",
 					tempLine = [],
+					tempChild = "",
 					rowSplit;
 
 				split.reverse();
 
-				for(var i = 0; i < split.length; i++){
-					split[i] = split[i].trim();
-					rowSplit = split[i].split("");
-					if(rowSplit.length > this.options.lineLength) {
-						for (var j = 0; j < rowSplit.length; j += this.options.lineLength) {
-							tempLine.push(rowSplit.slice(j,j+this.options.lineLength));
-						}
-						for (var k = tempLine.length - 1; k >= 0; k--) {
-							build += "<div class='line'"+this._difference(tempLine[k])+">" + tempLine[k].join("").replace(/(.)/g,"<div class='elem'>$1</div>") + "</div>";
+
+				for (var i = children.length - 1; i >= 0; i--) {
+					tempChild = children[i].textContent.trim();
+					if(tempChild !== null && !children[i].hasChildNodes()){
+						rowSplit = tempChild.split("");
+						if(rowSplit.length > this.options.lineLength) {
+							for (var j = 0; j < rowSplit.length; j += this.options.lineLength) {
+								tempLine.push(rowSplit.slice(j,j+this.options.lineLength));
+							}
+							for (var k = tempLine.length - 1; k >= 0; k--) {
+								build += "<div class='line'"+this._difference(tempLine[k])+">" + tempLine[k].join("").replace(/(.)/g,"<div class='elem'>$1</div>") + "</div>";
+							}
+						} else {
+							build += "<div class='line'"+this._difference(tempChild.trim().split(""))+">" + tempChild.replace(/(.)/g,"<div class='elem'>$1</div>") + "</div>";
 						}
 					} else {
-						build += "<div class='line'"+this._difference(split[i].trim().split(""))+">" + split[i].replace(/(.)/g,"<div class='elem'>$1</div>") + "</div>";
+						build += "<div class='line'>"+tempChild+"</div>";
 					}
 				}
 				build = build.replace(/「/g,"﹁");
