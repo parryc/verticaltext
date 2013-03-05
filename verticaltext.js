e45e8625e59d4c8aedfdb9e1ec7a7ea778ec3fd3
parryc <parrycadwallader@gmail.com>
2013-03-05 00:48:04 -0500

    Switch to preprocessing to allow for better handling of line-internal text

--- verticaltext.js
+++ verticaltext.js
@@ -23,41 +23,66 @@
 				}
 				this.setVertical();
 			},
-			_difference: function(array){
-				if(this.options.noOutsideWrap)
-					return " style='margin-bottom: "+(this.options.lineLength-array.length)+"em' ";
-				else
-					return "";
+			_preprocess: function(){
+				var children = this.children,
+					section = [],
+					text = [],
+					isSub, stub;
+				for (var i = 0; i < children.length; i++) {
+					if(children[i].localName === "br") {
+						text.push(section);
+						section = [];
+					} else {
+						stub = children[i].textContent.trim();
+						if(stub !== null && !children[i].hasChildNodes())
+							section.push({text: stub, isSub: false });
+						else
+							section.push({text: stub, isSub: true});
+						console.log(i + " " + stub);
+					}
+				}
+				if(section)
+					text.push(section);
+				return text.reverse();
 			},
 			setVertical: function(){
-				var split = this.rawHtml.split(/<br\/?>/),
-					children = this.children,
+				var processed = this._preprocess(),
 					build = "",
 					tempLine = [],
 					tempChild = "",
 					rowSplit;
 
-				split.reverse();
-
-
-				for (var i = children.length - 1; i >= 0; i--) {
+				processed.forEach(function(el,ind,array){
+					build += "<div class='line'>";
+					for (var i = 0; i < el.length; i++) {
+						if(el[i].isSub)
+							build += "<div class='other'>"+el[i].text+"</div>";
+						else
+							build += el[i].text.replace(/(.)/g,"<div class='elem'>$1</div>");
+					}
+					build += "</div>";
+				});
+				console.log(build);
+				/*for (var i = children.length - 1; i >= 0; i--) {
 					tempChild = children[i].textContent.trim();
-					if(tempChild !== null && !children[i].hasChildNodes()){
-						rowSplit = tempChild.split("");
-						if(rowSplit.length > this.options.lineLength) {
-							for (var j = 0; j < rowSplit.length; j += this.options.lineLength) {
-								tempLine.push(rowSplit.slice(j,j+this.options.lineLength));
-							}
-							for (var k = tempLine.length - 1; k >= 0; k--) {
-								build += "<div class='line'"+this._difference(tempLine[k])+">" + tempLine[k].join("").replace(/(.)/g,"<div class='elem'>$1</div>") + "</div>";
+
+					if(children[i].localName !== "br")
+						if(tempChild !== null && !children[i].hasChildNodes()){
+							rowSplit = tempChild.split("");
+							if(rowSplit.length > this.options.lineLength) {
+								for (var j = 0; j < rowSplit.length; j += this.options.lineLength) {
+									tempLine.push(rowSplit.slice(j,j+this.options.lineLength));
+								}
+								for (var k = tempLine.length - 1; k >= 0; k--) {
+									build += "<div class='line'>" + tempLine[k].join("").replace(/(.)/g,"<div class='elem'>$1</div>")+"</div>";
+								}
+							} else {
+								build += "<div class='line'>" + tempChild.replace(/(.)/g,"<div class='elem'>$1</div>")+"<div class='other'>parry</div></div>";
 							}
 						} else {
-							build += "<div class='line'"+this._difference(tempChild.trim().split(""))+">" + tempChild.replace(/(.)/g,"<div class='elem'>$1</div>") + "</div>";
+							build += "<div class='other'>"+tempChild+"</div>";
 						}
-					} else {
-						build += "<div class='line'>"+tempChild+"</div>";
-					}
-				}
+				}*/
 				build = build.replace(/「/g,"﹁");
 				build = build.replace(/」/g,"﹂");
 				document.querySelectorAll(options.selector)[0].innerHTML = build;
@@ -65,6 +90,7 @@
 			}
 		};
 	}
+
 	root.verticalizer = verticalizer();
 	root.verticalizer.setVertical();
 })(this);
